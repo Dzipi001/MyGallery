c13fdaa8e94908e2a7a7b1f53528deb8fa77cbfa
Goran Prunk <goranp@microsoft.com>
2017-12-20 19:44:26 -0800

    slideshow working, image zooming in/out

---
+++ OneDrivePhotoBrowser/FileManagement/Categories.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OneDrivePhotoBrowser
+{
+
+    public class CategoryManager
+    {
+        List<String> DefaultCategories = new List<string> { "Liked", "Disliked", "Kids", "Family", "Pets", "Travel"};
+
+        List<Category> Categories;
+
+        public CategoryManager()
+        {
+            Categories = new List<Category>();
+        }
+
+        public void Load(/*FileLocation???*/)
+        {
+            Categories.Clear();
+            //TODO - for now load locally
+            foreach(String cat in DefaultCategories)
+            {
+                Category newCategory = new Category(cat);
+                // TODO add any mock IDs Here!!!
+                // TODO add some mock SubCategories here!!!
+
+                Categories.Add(newCategory);
+            }
+        }
+
+        public List<String> GetCategory(String categoryName)
+        {
+            // TODO - can we load specific category only?
+
+            List<String> result = new List<string>();
+
+            return result;
+        }
+    }
+
+
+    public class Category
+    {
+        String Name;
+        List<Category> SubCategories;
+        List<String> IDs;
+
+        public Category(String name)
+        {
+            Name = name;
+            IDs = new List<string>();
+            SubCategories = new List<Category>();
+        }
+    }
+}
---
+++ OneDrivePhotoBrowser/FileManagement/Settings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Storage;
+
+namespace OneDrivePhotoBrowser
+{
+
+
+    public class Settings
+    {
+        ApplicationDataContainer roamingSettings = null;
+
+        public String ActiveCategory;
+        public String ActiveSubCategory;
+        public AppMode Mode;
+
+        public enum AppMode { RANDOM=1, RANDOM_WITH_LIKED, LIKED_ONLY, DISLIKED, CATEGORY_ONLY, CURRENT_FOLDER, SAME_DAY, SAME_MONTH, SAME_YEAR};
+
+        public Settings()
+        {
+            ActiveCategory = String.Empty;
+            ActiveSubCategory = String.Empty;
+            Mode = AppMode.RANDOM;
+
+        }
+
+        public void Load()
+        {
+            roamingSettings = ApplicationData.Current.RoamingSettings;
+
+            object setting = roamingSettings.Values["ActiveCategory"];
+            ActiveCategory = setting as String;
+
+            setting = roamingSettings.Values["ActiveSubCategory"];
+            ActiveSubCategory = setting as String;
+
+            setting = roamingSettings.Values["Mode"];
+            String ModeString = setting as String;
+            Mode = (AppMode)Enum.Parse(typeof(AppMode), ModeString);
+        }
+
+        public void Save()
+        {
+            roamingSettings = ApplicationData.Current.RoamingSettings;
+
+            roamingSettings.Values["ActiveCategory"] = ActiveCategory;
+            roamingSettings.Values["ActiveSubCategory"] = ActiveSubCategory;
+            roamingSettings.Values["Mode"] = Mode.ToString();
+        }
+    }
+
+}
